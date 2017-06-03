cb79c3dfddca1f3113b86a88d7413756b3f8b0f7
Hyubert <rusin.oleksander@yandex.ua>
2017-06-03 19:34:17 +0300

    Add files via upload

--- js/constructor.js
+++ js/constructor.js
@@ -73,25 +73,23 @@ function init() {
 
 	// grid
 
-	// var size = 500, step = 20;
+	var size = 500, step = 50;
 
-	// var geometry = new THREE.Geometry();
+	var geometry = new THREE.Geometry();
 
-	// for ( var i = - size; i <= size; i += step ) {
+	for ( var i = - size; i <= size; i += step ) {
 
-	// 	geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
-	// 	geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+		geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+		geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
 
-	// 	geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
-	// 	geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+		geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+		geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
 
-	// }
+	}
 
-	var geometry = new THREE.PlaneGeometry(window.innerWidth, 10, window.innerHeight)
+	var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
 
-	var material = new THREE.PointsMaterial( { map: new THREE.TextureLoader().load( 'img/lego.png' ) } );
-
-	var line = new THREE.Mesh( geometry, material );
+	var line = new THREE.LineSegments( geometry, material );
 	scene.add( line );
 
 	//
